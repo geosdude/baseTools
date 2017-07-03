20ef84d01bc63db9f3e4251908b647eeea4014f9
geosdude <geosdude@users.noreply.github.com>
2017-07-03 16:45:43 -0500

    __init__.py clean up.

--- lst/__init__.py
+++ lst/__init__.py
@@ -3,8 +3,4 @@
 # baseTools.lst.__init__
 # Coded by Richard Polk
 #----------------------------------------------------
-<<<<<<< HEAD
 # I am a package directory listed in import statements
-=======
-# I am a package directory listed in import statements
->>>>>>> db66a150ab94d0c527b84c9a975c6015df6ab731
